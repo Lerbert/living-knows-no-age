66e39e783cf1fc4dfd96e4162a82747786c518eb
Florian Lercher <florian.lercher@tum.de>
2024-11-23 11:44:40 +0100

    Add navbar with bootstrap

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Profile</h1>
+  <button class="btn btn-primary" @click="count++">count is: {{ count }}</button>
+</template>
+
+<style></style>
