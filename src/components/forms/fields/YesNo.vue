04a54e76a8ff4c5535bb100a7ff95bcb3c43655d
Florian Lercher <florian.lercher@tum.de>
2024-11-23 23:48:32 +0100

    Add icons for yes/no

--- src/components/forms/fields/YesNo.vue
+++ src/components/forms/fields/YesNo.vue
@@ -3,9 +3,9 @@
     <label class="form-label" id="title">{{ title }}</label>
     <div>
       <div class="d-flex align-items-center">
-        <div class="form-check me-4">
+        <div class="btn-group" role="group">
           <input
-            class="form-check-input"
+            class="btn-check"
             type="radio"
             :name="id"
             :id="`${id}-no`"
@@ -13,11 +13,22 @@
             v-model="radioSelection"
             @change="updateModel"
           />
-          <label class="form-check-label" :for="`${id}-no`">Nein</label>
-        </div>
-        <div class="form-check">
+          <label class="btn btn-outline-danger" :for="`${id}-no`">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              class="bi bi-x"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"
+              />
+            </svg>
+          </label>
           <input
-            class="form-check-input"
+            class="btn-check"
             type="radio"
             :name="id"
             :id="`${id}-yes`"
@@ -25,7 +36,20 @@
             v-model="radioSelection"
             @change="updateModel"
           />
-          <label class="form-check-label" :for="`${id}-yes`">Ja</label>
+          <label class="btn btn-outline-success" :for="`${id}-yes`">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              class="bi bi-check"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"
+              />
+            </svg>
+          </label>
         </div>
       </div>
     </div>
