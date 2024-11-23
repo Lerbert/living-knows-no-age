264f67de56915e0f81505f4d167ffa0f8102c900
Florian Lercher <florian.lercher@tum.de>
2024-11-23 20:34:49 +0100

    Add TS class for senior

---
+++ src/types/Senior.ts
@@ -0,0 +1,19 @@
+import type { StudentWishType } from './StudentWishType'
+import type { TaskType } from './TaskType'
+
+export class Student {
+  constructor(
+    public name: string,
+    public phone: string,
+    public birthdate: Date,
+    public preferredLocation: string,
+    public offers: StudentWishType[],
+    public wishes: TaskType[],
+    public allowSmokers: boolean,
+  ) {}
+
+  get age(): number {
+    const now = new Date()
+    return now.getFullYear() - this.birthdate.getFullYear()
+  }
+}
