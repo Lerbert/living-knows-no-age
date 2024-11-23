1f922f265b3451db0039f19a5daf26ac0fe0f107
Florian Lercher <florian.lercher@tum.de>
2024-11-23 15:18:52 +0100

    Add yes no component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,10 @@
-<template>Hello World!</template>
+<script setup lang="ts">
+import YesNo from '@/components/forms/fields/YesNo.vue'
+import { ref } from 'vue'
+
+const selected = ref()
+</script>
+
+<template>
+  <YesNo v-model="selected" title="Foo" /><span>{{ selected ? 'ja' : 'nein' }}</span>
+</template>
