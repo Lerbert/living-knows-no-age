cf848fc37f2ab5121f2b7d0d255d16fc6912a550
Florian Lercher <florian.lercher@tum.de>
2024-11-24 00:09:58 +0100

    Fix merge conflict in nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -39,21 +39,17 @@
                 </RouterLink>
               </li>
               <li>
-                <RouterLink class="dropdown-item" to="/student-registration">
+                <RouterLink class="dropdown-item" to="/older-person-registration">
                   Wohnraumgebende
                 </RouterLink>
               </li>
             </ul>
           </li>
           <li class="nav-item">
-<<<<<<< HEAD
             <RouterLink class="nav-link" to="/contract-creation">Vertrag erstellen</RouterLink>
           </li>
           <li class="nav-item">
             <RouterLink class="nav-link" to="/contract-generator">Vertrag erstellen</RouterLink>
-=======
-            <RouterLink class="nav-link" to="/older-person-registration">Older person Registration</RouterLink>
->>>>>>> 35eb704 (Version is now working)
           </li>
         </ul>
         <form class="d-flex">
